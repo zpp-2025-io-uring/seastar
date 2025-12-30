20025a84a70d1c1e31a3bec13701e03b9fc40d03
MrD4rkne <marcinsz.pv@gmail.com>
2025-12-30 11:07:37 +0100

    Add asymmetric uring backend setup and configuration
    Introduce CLI option to specify async workers cpuset
    
    The new option (async-workers-cpuset) is used to cpecify the CPUs on which workers will be spawned. The recommended way to use this option is to specify it alongside --cpuset. When specified with neither --cpuset nor --smp, it will remove the specified cpus from shard allocation. When --smp or --cpuset are specified, it is possible that worker and app cpusets overlap.
    
    Add asymmetric uring backend creation to smp::configure
    
    Determine the async worker and app cores cpumasks.
    
    Implement uring allocation in smp::configure
    
    For shards which represent a group of shards using a singular networking core, allocate the uring in smp::configure.
    This allows uring_fd passing to other shards.
    Add a barrier to ensure all uring_fds are existant.
    
    Use the new uring creation method in reactor_backend_asymmetric_uring constructor.
    
    Co-authored-by: MrD4rkne <marcinsz.pv@gmail.com>
    Co-authored-by: mikolajbilski <mjbilski@gmail.com>
    Co-authored-by: pixelkubek <czyszczon.kuba@gmail.com>
    Co-authored-by: witek-formanski <witekformanski@gmail.com>

--- src/core/reactor_backend.cc
+++ src/core/reactor_backend.cc
@@ -48,6 +48,7 @@
 #include <seastar/core/reactor.hh>
 #include <seastar/core/reactor_config.hh>
 #include <seastar/core/smp.hh>
+#include <seastar/core/shard_id.hh>
 #include <seastar/util/defer.hh>
 #include <seastar/util/read_first_line.hh>
 
@@ -2032,6 +2033,21 @@ try_create_asymmetric_uring(const std::variant<std::monostate, int, compile_safe
     }
 }
 
+unsigned
+select_worker_cpu(seastar::shard_id shard_id, const resource::cpuset& worker_cpus) {
+    SEASTAR_ASSERT(!worker_cpus.empty());
+    const size_t selected_cpu_rank = get_uring_group_id(shard_id, worker_cpus);
+    return *std::next(worker_cpus.cbegin(), selected_cpu_rank);
+}
+
+bool is_master_shard(seastar::shard_id shard_id, const resource::cpuset& worker_cpus) noexcept {
+    return shard_id < worker_cpus.size();
+}
+
+unsigned get_uring_group_id(seastar::shard_id shard_id, const resource::cpuset& worker_cpus) noexcept {
+    return shard_id % worker_cpus.size();
+}
+
 } // namespace uring
 
 class reactor_backend_asymmetric_uring final : public reactor_backend_uring_base {
@@ -2187,6 +2203,19 @@ std::vector<reactor_backend_selector> reactor_backend_selector::available() {
     }
     ret.push_back(reactor_backend_selector("epoll"));
     return ret;
+}
+
+async_worker_allocation reactor_backend_selector::allocate_async_workers(const resource::cpuset& async_workers_cpu_set, const resource::cpuset& cpu_set) const {
+#ifdef SEASTAR_HAVE_URING
+    if (_name == "asymmetric_io_uring") {
+        if (async_workers_cpu_set.empty()) {
+            throw std::runtime_error("No CPUs specified for asymmetric_io_uring workers. Please see --async-workers-cpuset option.");
+        }
+
+        return {async_workers_cpu_set, cpu_set};
+    }
+#endif
+    return {{}, cpu_set};  // Other backends don't need workers
 }
 
 }
